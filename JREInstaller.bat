fbbc4e0181f914da89a3105da4294f1a2df3a495
Asep Haryana <asepharyanalm001@gmail.com>
2025-01-20 20:39:06 +0700

    Replace PowerShell command with curl for downloading JRE installer in JREInstaller.bat

--- JREInstaller.bat
+++ JREInstaller.bat
@@ -5,7 +5,7 @@ setlocal
 set "JRE_INSTALLER=OpenJDK17U-jre_x64_windows_hotspot_17.0.13_11.zip"
 if not exist "%JRE_INSTALLER%" (
     echo File %JRE_INSTALLER% tidak ditemukan, mendownload...
-    curl -L -o "%JRE_INSTALLER%" "https://raw.githubusercontent.com/MythEclipse/ProjectBP1/refs/heads/main/OpenJDK17U-jre_x64_windows_hotspot_17.0.13_11.zip"
+    powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/MythEclipse/ProjectBP1/refs/heads/main/OpenJDK17U-jre_x64_windows_hotspot_17.0.13_11.zip' -OutFile '%JRE_INSTALLER%'"
     if %errorlevel% neq 0 (
         echo Gagal mendownload JRE.
         goto :END
