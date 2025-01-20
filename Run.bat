8ed8118b69e7fa14992a72a7445c18d6908c4c6f
Asep Haryana <asepharyanalm001@gmail.com>
2025-01-20 20:18:28 +0700

    a

--- Run.bat
+++ Run.bat
@@ -12,11 +12,11 @@ call JREInstaller.bat
 REM Set the path to the extracted JRE
 set "JRE_PATH=.\PortableJRE\jdk-17.0.13+11-jre"
 
-REM Check if ProjectBP1-1.0-SNAPSHOT.jar exists
-if not exist "ProjectBP1-1.0-SNAPSHOT.jar" (
-    echo ProjectBP1-1.0-SNAPSHOT.jar not found, downloading...
-    powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/MythEclipse/ProjectBP1Teori/refs/heads/main/ProjectBP1-1.0-SNAPSHOT.jar -OutFile ProjectBP1-1.0-SNAPSHOT.jar"
+REM Check if ProjectBP1Teori-1.0-SNAPSHOT exists
+if not exist "ProjectBP1Teori-1.0-SNAPSHOT" (
+    echo ProjectBP1Teori-1.0-SNAPSHOT not found, downloading...
+    powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/MythEclipse/ProjectBP1/refs/heads/main/ProjectBP1Teori-1.0-SNAPSHOT -OutFile ProjectBP1Teori-1.0-SNAPSHOT"
 )
 
 REM Run the packaged JAR file using the specified JRE
-"%JRE_PATH%\bin\java" -jar ".\ProjectBP1-1.0-SNAPSHOT.jar"
+"%JRE_PATH%\bin\java" -jar ".\ProjectBP1Teori-1.0-SNAPSHOT"
